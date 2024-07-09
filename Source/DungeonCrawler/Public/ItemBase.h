66e9d67ff46accdb8cfce979ec1dbb014ee8617f
Nicolo <lombino97@gmail.com>
2024-07-09 17:32:51 +0200

    add base of inventory

---
+++ Source/DungeonCrawler/Public/ItemBase.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ItemBase.generated.h"
+
+UENUM(BlueprintType)
+enum EItemType :uint8
+{
+	Weapon UMETA(DisplayName = "Patrolling"),
+	Collectable UMETA(DisplayName = "Fight")
+};
+
+UCLASS()
+class DUNGEONCRAWLER_API AItemBase : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AItemBase();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item")
+	FString ItemID;
+};
